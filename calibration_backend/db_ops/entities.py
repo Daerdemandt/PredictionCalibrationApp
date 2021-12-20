69e50a95f53afa90ee998cffd032f22121ac7ac4
Siarshai <searchingone37@yandex.ru>
2021-12-20 20:43:42 +0300

    Tests for correct error raising when adding or answering questions

--- calibration_backend/db_ops/entities.py
+++ calibration_backend/db_ops/entities.py
@@ -83,6 +83,18 @@ def initialize_schema(db):
             answer = db.Column(db.Integer, nullable=False)
             probability = db.Column(db.Integer, nullable=False)
 
+            @validates('user_id')
+            def validate_user_id(self, key, user_id):
+                if not Schema.YNQuestion.query.filter(Schema.User.user_id == user_id).first():
+                    raise AssertionError('Answer to question for unknown user')
+                return user_id
+
+            @validates('ynq_id')
+            def validate_ynq_id(self, key, ynq_id):
+                if not Schema.YNQuestion.query.filter(Schema.YNQuestion.ynq_id == ynq_id).first():
+                    raise AssertionError('Answer to unknown question')
+                return ynq_id
+
             @validates('answer')
             def validate_answer(self, key, answer):
                 if answer not in {0, 1, 2}:
--- calibration_backend/integration_tests.py
+++ calibration_backend/integration_tests.py
@@ -171,6 +171,25 @@ def test_create_questions_error_when_parameters_not_supplied(client):
     assert_question_in_response(response.get_json()["questions"], None, 0)
 
 
+def test_create_questions_error_when_duplicate(client):
+    qdata = {"question": "TestQ1", "answer": 1, "topic": "integration_testing"}
+    _ = client.post('/create_question', data=qdata)
+    response = client.post('/create_question', data=qdata)
+    assert response.status_code == 503
+    response = client.get('/get_questions?page=1&user_id=1')
+    assert_get_questions_correct_response(response)
+    assert_question_in_response(response.get_json()["questions"], qdata, 1)
+
+
+def test_create_questions_invalid_answer_error(client):
+    qdata = {"question": "TestQ1", "answer": 2, "topic": "integration_testing"}
+    response = client.post('/create_question', data=qdata)
+    assert response.status_code == 503
+    response = client.get('/get_questions?page=1&user_id=1')
+    assert_get_questions_correct_response(response)
+    assert len(response.get_json()["questions"]) == 0
+
+
 def test_answer_questions_happy_day(client_uq):
     response = client_uq.get('/get_questions?page=1&user_id=1')
     assert len(response.get_json()["questions"]) == 3
@@ -193,6 +212,55 @@ def test_answer_questions_happy_day(client_uq):
     assert len(response.get_json()["questions"]) == 2
     response = client_uq.get('/get_questions?page=1&user_id=2')
     assert len(response.get_json()["questions"]) == 1
+
+
+def test_answer_questions_unknown_parameters_ignored(client_uq):
+    response = client_uq.post('/answer_question', data={
+        "user_id": 1, "ynq_id": 1, "answer": 0, "probability": 55, "unknown": 42})
+    assert response.status_code == 200
+    response = client_uq.get('/get_questions?page=1&user_id=1')
+    assert len(response.get_json()["questions"]) == 2
+
+
+def test_answer_questions_unknown_user_error(client_uq):
+    response = client_uq.post('/answer_question', data={
+        "user_id": 42, "ynq_id": 1, "answer": 0, "probability": 55})
+    assert response.status_code == 503
+    response = client_uq.get('/get_questions?page=1&user_id=1')
+    assert len(response.get_json()["questions"]) == 3
+
+
+def test_answer_questions_unknown_question_error(client_uq):
+    response = client_uq.post('/answer_question', data={
+        "user_id": 42, "ynq_id": 1, "answer": 0, "probability": 55})
+    assert response.status_code == 503
+    response = client_uq.get('/get_questions?page=1&user_id=1')
+    assert len(response.get_json()["questions"]) == 3
+
+
+def test_answer_questions_invalid_answer_error(client_uq):
+    response = client_uq.post('/answer_question', data={
+        "user_id": 1, "ynq_id": 1, "answer": 3, "probability": 55})
+    assert response.status_code == 503
+    response = client_uq.get('/get_questions?page=1&user_id=1')
+    assert len(response.get_json()["questions"]) == 3
+
+
+def test_answer_questions_invalid_answer_error(client_uq):
+    response = client_uq.post('/answer_question', data={
+        "user_id": 1, "ynq_id": 1, "answer": 1, "probability": 51})
+    assert response.status_code == 503
+    response = client_uq.get('/get_questions?page=1&user_id=1')
+    assert len(response.get_json()["questions"]) == 3
+
+
+def test_answer_questions_duplicate_answer_error(client_uq):
+    response = client_uq.post('/answer_question', data={
+        "user_id": 1, "ynq_id": 1, "answer": 1, "probability": 55})
+    assert response.status_code == 200
+    response = client_uq.post('/answer_question', data={
+        "user_id": 1, "ynq_id": 1, "answer": 0, "probability": 65})
+    assert response.status_code == 503  # still duplicate
 
 
 if __name__ == "__main__":
