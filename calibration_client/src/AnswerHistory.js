93d05b7d4ca6cda0051df16e1e51a5cb4337fa62
Siarshai <searchingone37@yandex.ru>
2021-12-23 19:12:39 +0300

    Use MUI Table for AnswerHistory

--- calibration_client/src/AnswerHistory.js
+++ calibration_client/src/AnswerHistory.js
@@ -1,60 +1,62 @@
 import { answerToString } from "./QuestionsPage/Answer";
-import styled from "styled-components";
 import React from "react";
 import axios from "axios";
 import prettifyResponseError from "./shared/prettifyResponseError";
-import { Button, Typography } from "@material-ui/core";
+import {
+  Button,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
-
-const StyledItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding-bottom: 5px;
-`;
-
-const StyledColumn = styled.span`
-  padding: 0 5px;
-  white-space: nowrap;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-
-  a {
-    color: inherit;
-  }
-
-  width: ${(props) => props.width};
-`;
 
 export const AnswerHistoryTable = ({ answerHistory }) => {
   return (
     <main>
-      <StyledItem style={{ paddingTop: 15 }}>
-        <StyledColumn width="33%">
-          <strong>Вопрос</strong>
-        </StyledColumn>
-        <StyledColumn width="12%">
-          <strong>Ваш ответ</strong>
-        </StyledColumn>
-        <StyledColumn width="12%">
-          <strong>На самом деле</strong>
-        </StyledColumn>
-        <StyledColumn width="33%">
-          <strong>Комментарий</strong>
-        </StyledColumn>
-      </StyledItem>
-      {answerHistory.map((answer) => (
-        <StyledItem key={answer.question}>
-          <StyledColumn width="33%">{answer.question}</StyledColumn>
-          <StyledColumn width="12%">
-            {answerToString(answer.user_answer)}
-          </StyledColumn>
-          <StyledColumn width="12%">
-            {answerToString(answer.real_answer)}
-          </StyledColumn>
-          <StyledColumn width="33%">{answer.comment}</StyledColumn>
-        </StyledItem>
-      ))}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 920 }} aria-label="answer history table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">
+                <strong>Вопрос</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>Ваш ответ</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>На самом деле</strong>
+              </TableCell>
+              <TableCell align="center">
+                <strong>Комментарий</strong>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {answerHistory.map((answer) => (
+              <TableRow
+                key={answer.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {answer.question}
+                </TableCell>
+                <TableCell align="center">
+                  {answerToString(answer.user_answer)}
+                </TableCell>
+                <TableCell align="center">
+                  {answerToString(answer.real_answer)}
+                </TableCell>
+                <TableCell align="right">{answer.comment}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </main>
   );
 };
@@ -91,7 +93,7 @@ export const AnswerHistory = ({ user }) => {
         if (error !== "") return <Typography variant="h4">{error}</Typography>;
         if (answerHistory.length === 0)
           return (
-            <Typography>
+            <Typography variant="h4">
               Пока что вы ещё не ответили ни на один вопрос
             </Typography>
           );
