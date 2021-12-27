4794ad699747a36b170b4624909fa75660b472bb
Siarshai <searchingone37@yandex.ru>
2021-12-27 10:14:12 +0300

    Fix get_statistics tests after moving real statistics computing to client-side

--- calibration_backend/calibration_backend.py
+++ calibration_backend/calibration_backend.py
@@ -74,11 +74,13 @@ def initialize_request_handlers(app, db, Schema):
             datapoints = Schema.query_answers_statistics(user_id)
         except Exception as e:
             return {"error": f"Could not query statistics data: {str(e)}"}, 503
+
         def clean_from_dontknows(dps):
             return [{"real_answer": dp["real_answer"],
-                   "user_answer": bool(dp["user_answer"]),
-                   "probability": dp["probability"]}
-                  for dp in dps if dp["user_answer"] in {0, 1}]
+                     "user_answer": bool(dp["user_answer"]),
+                     "probability": dp["probability"]}
+                    for dp in dps if dp["user_answer"] in {0, 1}]
+
         return {"statistics": clean_from_dontknows(datapoints)}
 
     @app.route("/get_answers", methods=['GET'])
--- calibration_backend/integration_tests/test_statistics_interaction.py
+++ calibration_backend/integration_tests/test_statistics_interaction.py
@@ -2,36 +2,31 @@ def assert_valid_statistics_response(response):
     assert response.status_code == 200
     jresp = response.get_json()
     assert "statistics" in jresp
+    expected_keys = {
+        "real_answer",
+        "user_answer",
+        "probability",
+    }
     for chunk in jresp["statistics"]:
-        assert all(
-            (field in chunk.keys() for field in {"correct_percent", "probability_quotient", "total", "total_correct"}))
+        assert set(chunk.keys()) == expected_keys
+
+
+def assert_uqa_expected_data(statistics):
+    expected = [
+        {"probability": 55, "real_answer": True, "user_answer": False},
+        {"probability": 55, "real_answer": False, "user_answer": False},
+        {"probability": 99, "real_answer": True, "user_answer": True},
+    ]
+    assert len(statistics) == len(expected)
+    for chunk in statistics:
+        assert chunk in expected, f"{chunk} not found in expected results or got it twice"
+        expected.remove(chunk)
 
 
 def test_statistics_empty_stats(client_uq):
     response = client_uq.get("/statistics?user_id=1")
     assert_valid_statistics_response(response)
-    for chunk in response.get_json()["statistics"]:
-        assert chunk["correct_percent"] is None
-        assert chunk["total"] == 0
-        assert chunk["total_correct"] == 0
-
-
-def test_statistics_happy_day(client_uqa):
-    response = client_uqa.get("/statistics?user_id=1")
-    assert_valid_statistics_response(response)
-    for chunk in response.get_json()["statistics"]:
-        if chunk["probability_quotient"] == 55:
-            assert chunk["correct_percent"] == 50
-            assert chunk["total"] == 2
-            assert chunk["total_correct"] == 1
-        elif chunk["probability_quotient"] == 99:
-            assert chunk["correct_percent"] == 100
-            assert chunk["total"] == 1
-            assert chunk["total_correct"] == 1
-        else:
-            assert chunk["correct_percent"] is None
-            assert chunk["total"] == 0
-            assert chunk["total_correct"] == 0
+    assert len(response.get_json()["statistics"]) == 0
 
 
 def test_statistics_no_user_id_provided_error(client_uq):
@@ -44,18 +39,20 @@ def test_statistics_unknown_user_returns_empty_result(client_uq):
     # that answer shouldn't have been added in the first place
     response = client_uq.get("/statistics?user_id=42")
     assert_valid_statistics_response(response)
-    for chunk in response.get_json()["statistics"]:
-        assert chunk["correct_percent"] is None
-        assert chunk["total"] == 0
-        assert chunk["total_correct"] == 0
+    assert len(response.get_json()["statistics"]) == 0
 
 
-def test_statistics_unknown_parameter_ignored(client_uq):
+def test_statistics_happy_day(client_uqa):
+    response = client_uqa.get("/statistics?user_id=1")
+    assert_valid_statistics_response(response)
+    statistics = response.get_json()["statistics"]
+    assert_uqa_expected_data(statistics)
+
+
+def test_statistics_unknown_parameter_ignored(client_uqa):
     # Does not check if user actually exists because
     # that answer shouldn't have been added in the first place
-    response = client_uq.get("/statistics?user_id=1&unknown_parameter=42")
+    response = client_uqa.get("/statistics?user_id=1&unknown_parameter=42")
     assert_valid_statistics_response(response)
-    for chunk in response.get_json()["statistics"]:
-        assert chunk["correct_percent"] is None
-        assert chunk["total"] == 0
-        assert chunk["total_correct"] == 0
+    statistics = response.get_json()["statistics"]
+    assert_uqa_expected_data(statistics)
