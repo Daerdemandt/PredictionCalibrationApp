3b68a14b1157c7f88036ef18adb98107f2537e58
Siarshai <searchingone37@yandex.ru>
2021-12-27 10:22:54 +0300

    Made get_answers tests more robust

--- calibration_backend/integration_tests/test_get_answers.py
+++ calibration_backend/integration_tests/test_get_answers.py
@@ -5,9 +5,48 @@ def assert_valid_get_answers_response(response):
     assert response.status_code == 200
     jresp = response.get_json()
     assert "answers" in jresp
+    expected_keys = {
+        "question",
+        "comment",
+        "real_answer",
+        "user_answer",
+        "probability",
+    }
     for chunk in jresp["answers"]:
-        assert all((field in chunk.keys() for field in {
-            "question", "user_answer", "probability", "real_answer", "comment", }))
+        assert set(chunk.keys()) == expected_keys
+
+
+def assert_uqa_expected_data(answers):
+    expected_data = {
+        "TestQ1": {
+            "real_answer": True,
+            "user_answer": 0,
+            "probability": 55,
+            "comment": "",
+        },
+        "TestQ2": {
+            "real_answer": False,
+            "user_answer": 0,
+            "probability": 55,
+            "comment": "",
+        },
+        "TestQ3": {
+            "real_answer": True,
+            "user_answer": 1,
+            "probability": 99,
+            "comment": "test_comment",
+        },
+        "TestQ5": {
+            "real_answer": False,
+            "user_answer": 2,
+            "probability": -1,
+            "comment": "",
+        }
+    }
+    assert len(answers) == len(expected_data.keys())
+    for name, values in expected_data.items():
+        answer_record = find_result_by_name(answers, name)
+        assert_values_equal(answer_record, values)
 
 
 def find_result_by_name(answers, name):
@@ -26,35 +65,14 @@ def test_get_answers_happy_day(client_uqa):
     response = client_uqa.get("/get_answers?user_id=1")
     assert_valid_get_answers_response(response)
     answers = response.get_json()["answers"]
-    assert len(answers) == 4
-    answer_record = find_result_by_name(answers, "TestQ1")
-    assert_values_equal(answer_record, {
-        "real_answer": True,
-        "user_answer": 0,
-        "probability": 55,
-        "comment": "",
-    })
-    answer_record = find_result_by_name(answers, "TestQ2")
-    assert_values_equal(answer_record, {
-        "real_answer": False,
-        "user_answer": 0,
-        "probability": 55,
-        "comment": "",
-    })
-    answer_record = find_result_by_name(answers, "TestQ3")
-    assert_values_equal(answer_record, {
-        "real_answer": True,
-        "user_answer": 1,
-        "probability": 99,
-        "comment": "test_comment",
-    })
-    answer_record = find_result_by_name(answers, "TestQ5")
-    assert_values_equal(answer_record, {
-        "real_answer": False,
-        "user_answer": 2,
-        "probability": -1,
-        "comment": "",
-    })
+    assert_uqa_expected_data(answers)
+
+
+def test_get_answers_unknown_parameter_ignored(client_uqa):
+    response = client_uqa.get("/get_answers?user_id=1&unknown_parameter=42")
+    assert_valid_get_answers_response(response)
+    answers = response.get_json()["answers"]
+    assert_uqa_expected_data(answers)
 
 
 def test_get_answers_no_user_id_provided_error(client_uqa):
@@ -69,7 +87,7 @@ def test_get_answers_no_such_user_empty_result(client_uqa):
     assert len(answers) == 0
 
 
-def test_get_answers_knonw_user_no_answers_empty_result(client_uq):
+def test_get_answers_known_user_no_answers_empty_result(client_uq):
     response = client_uq.get("/get_answers?user_id=1")
     assert_valid_get_answers_response(response)
     answers = response.get_json()["answers"]
